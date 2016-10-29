6b69c98144c43a7b40fcf7f63d200b4325920db7
roshanvrazak <roshanvrazak@gmail.com>
2016-10-29 15:35:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/biodata.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'biodata.html'));
+app.get('/ui/website.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'website.html'));
 });
 
 
